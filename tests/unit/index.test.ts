12f4f512aa0963b2362f14368573f016117cd99e
karan sivarat <bomb0069@gmail.com>
2020-03-22 11:05:56 +0700

    [TEST]: build pipeline when you commit wrong code style

--- tests/unit/index.test.ts
+++ tests/unit/index.test.ts
@@ -4,7 +4,7 @@ import { equal } from 'assert'
 describe('hello in index file', () => {
   it('hello without parameter will return Hello World!',
     () => {
-      const expected = 'Hello World!'
+      const expected = 'Hello World!';
       const actual = hello()
       equal(actual, expected)
     })
